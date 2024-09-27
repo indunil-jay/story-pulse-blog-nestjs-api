ecfc341d23cc367bb2ff3acec777dbac68d6997e
indunil-jay <indunil.jayz@gmail.com>
2024-09-27 15:52:58 +0530

    config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        synchronize: true, //TODO: use only dev
+        entities: [],
+        port: 5432,
+        username: 'postgres',
+        password: 'root1122',
+        host: 'localhost',
+        database: 'story-pulse',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
